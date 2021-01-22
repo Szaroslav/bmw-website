d135974926615529edb506425396554fc5740844
Jakub Szaredko <jakub.szaredko@gmail.com>
2021-01-22 20:28:50 +0100

    Delete the section and create a main content component

--- src/Bmw.js
+++ src/Bmw.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from './Header.js';
-import DescSection from './DescSection.js';
+import MainContent from './MainContent.js';
 
 class Bmw extends React.Component {
     render = () => {
         return (
             <div className="bmw-website">
                 <Header name="bmw"/>
-                <main>
-                    <DescSection contentPos='right' />
-                    <DescSection contentPos='left' />
-                </main>
+                <MainContent />
             </div>
         );
     }
---
+++ src/MainContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <main>
+            <section>
+                <h3>FAKTY O NOWYM BMW iX.</h3>
+            </section>
+            <section>
+                <h3>INNOWACYJNA AERODYNAMIKA BMW iX.</h3>
+                <p>
+                    BMW iX w każdym milimetrze swojej dynamicznej stylistyki wdraża zasadę „forma podąża za funkcją”.
+                </p>
+                <p>
+                    Widać to przede wszystkim po gładkiej karoserii ze zwężoną nadbudową kabiny i zredukowanymi fugami 
+                    oraz po płaskim podwoziu. W połączeniu z zamkniętym przodem oraz dyfuzorem z tyłu i niezwykłymi detalami, 
+                    takimi jak wpuszczane klamki czy zintegrowane z listwami szybowymi lusterka zewnętrzne, BMW iX osiąga 
+                    doskonały współczynnik Cx wynoszący zaledwie 0,25. Dzięki temu łączy w sobie zasięg i wydajność 
+                    z innowacyjną konstrukcją SAV-a.
+                </p>
+            </section>
+        </main>
+    );
+}
+
+export default MainContent;
