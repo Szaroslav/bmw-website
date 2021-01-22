c3a75b9a8ff92f533289a2b8e65409afcd365de4
Jakub Szaredko <jakub.szaredko@gmail.com>
2021-01-22 19:11:41 +0100

    Create a main container and sections

--- src/Bmw.js
+++ src/Bmw.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from './Header.js';
+import DescSection from './DescSection.js';
 
-class Bwm extends React.Component {
+class Bmw extends React.Component {
     render = () => {
         return (
             <div className="bmw-website">
                 <Header name="bmw"/>
+                <main>
+                    <DescSection contentPos='right' />
+                    <DescSection contentPos='left' />
+                </main>
             </div>
         );
-
     }
 }
 
-export default Bwm;
+export default Bmw;
---
+++ src/DescSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DescSection.scss';
+
+const DescSection = ({ contentPos }) => {
+
+    return (
+        <section className={'bmw-desc-section content-' + contentPos}>
+
+        </section>
+    );
+}
+
+export default DescSection;
