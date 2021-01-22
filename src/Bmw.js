3012e20e6079cde36b327a35d615e24eb501e5ae
Jakub Szaredko <jakub.szaredko@gmail.com>
2021-01-22 21:43:50 +0100

    Create a test drive form component

--- src/Bmw.js
+++ src/Bmw.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header.js';
 import MainContent from './MainContent.js';
 import Gallery from './Gallery.js';
+import TestDriveForm from './TestDriveForm.js';
 
 class Bmw extends React.Component {
     render = () => {
@@ -10,6 +11,7 @@ class Bmw extends React.Component {
                 <Header name="bmw"/>
                 <MainContent />
                 <Gallery />
+                <TestDriveForm />
             </div>
         );
     }
---
+++ src/TestDriveForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TestDriveForm.scss';
+
+const TestDriveForm = () => {
+    return (
+        <section className='bmw-test-drive-form'>
+            
+        </section>
+    );
+}
+
+export default TestDriveForm;
