a820950fdafda729c943c3eea3b86b361ff58554
Jakub Szaredko <jakub.szaredko@gmail.com>
2021-01-22 14:56:35 +0100

    Add SCSS

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.scss';
 
 const Header = (props) => {
     return (
         <header className={props.name + "-header"}>
-            
+            <img src="./assets/header-slider.jpg" alt="header-slider-1" />
         </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bmw from './Bmw.js';
+import './index.css';
 
 ReactDOM.render(<Bmw />, document.getElementById('root'));
