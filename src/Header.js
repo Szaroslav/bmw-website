aea3776b13dfb8b3f2b3dfde78e60b23ca1dbc71
Jakub Szaredko <jakub.szaredko@gmail.com>
2021-01-22 15:49:32 +0100

    Update the header files, add a BMW logotype

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Header.scss';
+import { ReactComponent as BmwLogotype } from './assets/bmw-logotype.svg';
 
 const Header = (props) => {
     return (
-        <header className={props.name + "-header"}>
-            <img src="./assets/header-slider.jpg" alt="header-slider-1" />
+        <header className={props.name + '-header'}>
+            <div className={props.name + '-header-slider'}>
+                <img src='./assets/header-slider.jpg' alt='header-slider-1' />
+                <div className='bmw-header-slider-btn-ctn'>
+                    <button className='bmw-header-slider-btn'></button>
+                    <button className='bmw-header-slider-btn'></button>
+                    <button className='bmw-header-slider-btn'></button>
+                </div>
+            </div>
+            <div className='bmw-header-top-content'>
+                <nav className={props.name + '-header-nav bmw-row'}>
+                    <ul>
+                        <li>Nowe BMW iX</li>
+                        <li>Umów jazdę próbną</li>
+                    </ul>
+                    <BmwLogotype />
+                </nav>
+                <div className='bmw-row'>
+                    <div>
+                        <p>Radość z jazdy</p>
+                    </div>
+                </div>
+            </div>
+            <div className={props.name + '-header-content'}>
+                <h2>NOWE BMW iX</h2>
+                <p>PIONIER NOWEJ ERY</p>
+                <button>UMÓW JAZDĘ PRÓBNĄ</button>
+            </div>
         </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bmw from './Bmw.js';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(<Bmw />, document.getElementById('root'));
